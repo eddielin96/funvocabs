b3ffae654e9d5a2c0a06226b75f47c1aaa59c11d
Eddie Lin <heavenly0216@gmail.com>
2020-11-23 15:05:17 +0800

    set up funvocabs project

---
+++ header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+    integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <style>
+    body{
+        background:#33bbff;
+    }
+    form > div > p > input{
+        background:#f2f2f2;
+        border: 0px;
+        border-bottom: 1px solid #eeeeee;
+        color:#000000;
+        font-family:"Comic Sans MS", "Arial";
+    }
+    ::placeholder{
+        color:#eeeeee;
+    }
+/*
+    form > div > p > input:invalid{
+        background-color:red;
+        border:none;
+    }
+    form > div > p > input:valid{
+        background-color:#80ffaa;
+        border:none;
+    }
+*/
+    </style>
+</head>
---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+include_once('header.php');
+?>
+
+<body>
+<div class="container col-10">
+<div class="d-flex justify-content-center align-items-center" style="height:90vh">
+    
+    <form action="check.php" method="post">
+    <h5 class="text-center text-white" >Sign in</h5>
+        <div class="my-3">
+            <p><input type="text" name="acc" required placeholder="Username" style="width:100%" minlength="6" maxlength="10"></p>
+            <p  class="my-3"></p>
+        </div>
+        <div class="my-3">
+            <p><input type="password" name="pw" required placeholder="Password " style="width:100%" minlength="6" maxlength="12"></p>
+            <p class="my-3"></p>
+        </div>
+        <div>
+            <a href="forget_pw.php">Forget password?</a>
+            <a href="regiser.php">Create an account</a>
+        </div>
+        <div class="mt-3">
+            <input class="font-weight-normal" type="submit" value="SIGN IN" style="width:100%; border:none; background:#e1e1ea">
+        </div>
+    </form>
+</div>
+</div>
+
+<?php
+include_once('footer.php');
+?>
