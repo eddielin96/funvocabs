142f66b3689d82fc33bfeb840194206869c2a4a4
Eddie Lin <edward.ct.lin@gmail.com>
2020-11-27 22:42:32 +0800

    connect mysql database, debug text with ' problem and emoji icon problem

---
+++ api/insert_new_words.php
@@ -0,0 +1,83 @@
+<?php
+include_once "../base.php";
+echo "<pre>";
+print_r(array_keys($_POST));
+echo "<pre>";
+
+$voc=$_POST['vocab'];
+$type=$_POST['type'];
+$meaning=$_POST['meaning'];
+$sentence=$_POST['sentence'];
+$note=$_POST['note'];
+
+echo $sentence;
+
+echo "<hr>";
+
+/* 避免輸入英文句子時或筆記時，內容包含'符號，導致匯入回資料庫時出錯，
+利用條件判斷使用者輸入的內容的句子或筆記內容中有'符號時，在前加上\(反斜線)轉譯特殊符號*/
+
+//避免單字出現' 影響sql 語句輸入
+if (strpos("$voc","'")){
+    $a = explode("'",$voc);
+    $voc= implode("\'",$a);
+    echo "已加入\在'符號前";
+ }else{
+     //if 輸入字串原本就沒有任何'符號 do nothing
+ }
+
+ //避免意思出現' 影響sql 語句輸入
+if (strpos("$meaning","'")){
+    $a = explode("'",$meaning);
+    $meaning= implode("\'",$a);
+    echo "已加入\在'符號前";
+ }else{
+     //if 輸入字串原本就沒有任何'符號 do nothing
+ }
+
+//避免句子出現' 影響sql 語句輸入
+if (strpos("$sentence","'")){
+   $a = explode("'",$sentence);
+   $sentence= implode("\'",$a);
+}else{
+    //if 輸入字串原本就沒有任何'符號 do nothing
+}
+
+//避免筆記出現' 影響sql 語句輸入
+if (strpos("$note","'")){
+    $a = explode("'",$note);
+    $note= implode("\'",$a);
+    echo "已加入\在'符號前";
+ }else{
+     //if 輸入字串原本就沒有任何'符號 do nothing
+ }
+
+ // $a 為使用一次的變數，所以重複使用沒有關係
+
+ 
+// echo $voc;
+// echo "<br>";
+// echo $type;
+// echo "<br>";
+// echo $meaning;
+// echo "<br>";
+// echo $sentence;
+// echo "<br>";
+// echo $note;
+// echo "<br>";
+
+$sql="insert into 
+vocabs
+(`voc`,`type`,`meaning`,`sentence`,`note`)
+values
+('$voc','$type','$meaning','$sentence','$note')";
+
+
+
+$pdo->exec($sql);
+
+echo "<br>";
+echo $sql;
+
+
+?>
--- base.php
+++ base.php
@@ -1,8 +1,19 @@
 <?php
 
-$dsn="mysql:host=localhost;dbname=funvocabs;charset=utf8;";
+$dsn="mysql:host=localhost;dbname=funvocabs;charset=utf8mb4;";
 $pdo=new PDO($dsn,'root','');
+// utf8 改成 utf8mb4 為了怕筆記內容有表情符號出現亂碼存不進去資料庫故更改
+/* 另外也在mysql輸入以下語法改變編碼 以讓mysql 可以判讀emoji 
 
+
+ALTER TABLE
+
+    table_name <- 輸入資料表的名稱
+
+    CONVERT TO CHARACTER SET utf8mb4
+
+    COLLATE utf8mb4_unicode_ci;
+ */
 date_default_timezone_set("Asia/Taipei");
 
 
